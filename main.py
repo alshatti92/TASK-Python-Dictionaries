4f6777a68398648cc8d1effb7ad0df927353da9d
alshatti92 <a.alshatti@hotmail.co.uk>
2022-09-26 19:33:09 +0300

    pending_challange

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # Create a dictionary called `person`, it has three properties, `name` which is a string, `age` which is an integer, `hobbies` which is a list of strings.
-
+person = {
+    "name": "abdullah",
+    "age": 30,
+    "hobbies": ["playing", "reading", "eating"]
+}
+print (person)
 
 # Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
 
 
 def change_age(person, number):
-    ...
+    person ["age"] = number
+    return person
 
 
 print(change_age(person, 21))
@@ -15,7 +21,8 @@ print(change_age(person, 21))
 
 
 def add_hobby(person, hobby):
-    ...
+    person ["hobbies"].append(hobby)
+    return person
 
 
 print(add_hobby(person, "Table Tennis"))
